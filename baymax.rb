6ff9917a045df248fbf3e804c0dee4590edf9e73
Dhananjay Puglia <dhansiddh@gmail.com>
2016-02-23 14:04:59 +0530

    Update baymax.rb

--- baymax.rb
+++ baymax.rb
@@ -4,7 +4,7 @@ class Baymax < Formula
   url "http://githuben.intranet.mckinsey.com/Appify/Baymax/archive/1.0.0.tar.gz"
   version "1.0.0"
   sha256 "a1d03dc0ecb52a4d6abc4627f8de10d47568b7913b15115cc1ee67d0d8fb0060"
-
+  depends_on :x11 
   def install
     bin.install "Baymax"
     prefix.install Dir["Source"]
