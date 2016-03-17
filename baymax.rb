e4b61a8746f4173e0922d8f94c7edb97ce5862ac
Dhananjay Puglia <dhansiddh@gmail.com>
2016-03-17 17:04:31 +0530

    Update baymax.rb

--- baymax.rb
+++ baymax.rb
@@ -2,7 +2,7 @@ class Baymax < Formula
   desc ""
   homepage ""
   url "http://githuben.intranet.mckinsey.com/Appify/Baymax/archive/1.0.1.tar.gz"
-  version "1.0.0"
+  version "1.0.1"
   sha256 "1dac91d36b766de9954fe9b503f195bdbd470e3decd9c4f1b0477ea7e37a5b52"
   depends_on :x11 
   def install
